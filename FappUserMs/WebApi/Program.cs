f889023027f2e381f374a352ffdfd0ebb7eeab65
alexandreakdeniz <alexandre.akdeniz@free.fr>
2023-08-01 17:11:20 +1200

    Add the other 3 projects

--- FappUserMs/WebApi/Program.cs
+++ FappUserMs/WebApi/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +6,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+const string allowSpecificOrigins = "_allowSpecificOrigins"; // The name of the CORS policy.
+builder.Services
+    .AddCors(options =>
+    {
+        options.AddPolicy(allowSpecificOrigins,
+            bld =>
+            {
+                bld.WithOrigins("*")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+                    .WithExposedHeaders("Content-Disposition");
+            });
+    });
+
 
 var app = builder.Build();
 
@@ -16,8 +32,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(allowSpecificOrigins);
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
